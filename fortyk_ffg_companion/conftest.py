b43abc0a7046dded4f7db14dd0de0494e233a1fe
Anton Savill <notna888+git@gmail.com>
2020-12-30 18:24:09 +0800

    initial commit

---
+++ fortyk_ffg_companion/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from fortyk_ffg_companion.users.models import User
+from fortyk_ffg_companion.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
